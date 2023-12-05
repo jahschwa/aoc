e252d251be758e37b1934f751dc70cfede594a36
Joshua Haas <haas.josh.a@gmail.com>
2023-12-04 20:26:33 -0500

    new: Day01b_jfred

--- 2023/solution/day_01b.py
+++ 2023/solution/day_01b.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import re
 
 from day_01a import Day01a
 
@@ -41,3 +42,40 @@ class Day01b(Day01a):
           return num
 
     raise RuntimeError('no number found in "{}"'.format(''.join(line)))
+
+
+class Day01b_jfred(Day01b):
+
+  def __init__(self):
+
+    self.words = {word: str(idx + 1) for (idx, word) in enumerate(self.WORDS)}
+    self.regex = re.compile('|'.join(self.words))
+    self.shortest = len(min(self.words))
+
+  def decode(self, line):
+
+    line = self.replace_words(line)
+    return super().decode(line)
+
+  def first_num(self, line, reverse=False):
+
+    return super(Day01b, self).first_num(line, reverse)
+
+  def replace_words(self, line):
+
+    result = []
+    idx = 0
+    while True:
+      match = self.regex.search(line, idx)
+      if not match:
+        break
+      (word, end) = (match.group(), match.end())
+      result += [
+        line[idx : match.start()],
+        self.words[word],
+      ]
+      idx = end - 1
+
+    result.append(line[idx :])
+
+    return ''.join(result)

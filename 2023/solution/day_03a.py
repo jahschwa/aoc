db1c6eca6f1c5def445260f1657597d68312f5bb
Joshua Haas <haas.josh.a@gmail.com>
2023-12-05 20:34:02 -0500

    puzzle: Day03a, Day03b

---
+++ 2023/solution/day_03a.py
@@ -0,0 +1,97 @@
+from dataclasses import dataclass, field
+
+from base import Solution
+
+
+class Day03a(Solution):
+
+  SOLUTION = 532331
+
+  def solve(self, lines):
+
+    return sum(part.number for part in Schematic.parse(lines))
+
+
+@dataclass
+class Part:
+
+  row: int
+  start_col: int
+  end_col: int
+  number: int
+
+
+class Schematic:
+
+  IGNORE = '.'
+
+  def __init__(self, parts, symbols):
+
+    self.parts = parts
+    self.symbols = symbols
+
+  @classmethod
+  def parse(cls, lines):
+
+    parts = []
+    symbols = {}
+
+    def add_part(row, end_col, num):
+      parts.append(
+        Part(row, end_col - len(num) + 1, end_col, int(num))
+      )
+
+    for (row, line) in enumerate(lines):
+      current = []
+      for (col, c) in enumerate(line):
+        if c.isdigit():
+          current.append(c)
+          continue
+        if c not in Schematic.IGNORE:
+          symbols[(row, col)] = Symbol(row, col, c)
+        if current:
+          add_part(row, col - 1, ''.join(current))
+          current = []
+      if current:
+        add_part(row, col - 1, ''.join(current))
+
+    return cls(parts, symbols)
+
+  def adjacent_symbols(self, part, limit=None, allow=None):
+
+    symbols = {}
+    (left, right) = (part.start_col - 1, part.end_col + 1)
+
+    def add_symbol(row, col):
+      if (
+        (symbol := self.symbols.get((row, col)))
+        and (not allow or symbol.char in allow)
+      ):
+          symbols[(row, col)] = symbol
+
+    for row in (part.row - 1, part.row + 1):
+      for col in range(left, right + 1):
+          add_symbol(row, col)
+          if limit and len(symbols) >= limit:
+            return symbols
+
+    row = part.row
+    for col in (left, right):
+      add_symbol(row, col)
+      if limit and len(symbols) >= limit:
+        return symbols
+
+    return symbols
+
+  def __iter__(self):
+
+    for part in self.parts:
+      if self.adjacent_symbols(part, 1):
+        yield part
+
+
+@dataclass(frozen=True)
+class Symbol:
+  row: int
+  col: int
+  char: str = field(compare=False)
---
+++ 2023/solution/day_03b.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+from dataclasses import dataclass
+
+from day_03a import Day03a, Part, Schematic
+
+
+class Day03b(Day03a):
+
+  SOLUTION = 82301120
+
+  def solve(self, lines):
+
+    return sum(gear.ratio() for gear in SchematicB.parse(lines))
+
+
+class SchematicB(Schematic):
+
+  GEAR = '*'
+
+  def __iter__(self):
+
+    parts_near_gears = defaultdict(lambda: [])
+    for part in self.parts:
+      for symbol in self.adjacent_symbols(part, None, SchematicB.GEAR):
+        parts_near_gears[symbol].append(part)
+
+    for parts in parts_near_gears.values():
+      if len(parts) == 2:
+        yield Gear(*parts)
+
+
+@dataclass
+class Gear:
+
+  part1: Part
+  part2: Part
+
+  def ratio(self):
+
+    return self.part1.number * self.part2.number
